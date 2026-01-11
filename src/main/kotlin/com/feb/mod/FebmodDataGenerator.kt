410c9e9826df76d8104fcb4f75c77d591b9af826
Februari10 <febban2010@gmail.com>
2026-01-11 22:16:02 +0100

    FebMod is so tuff

---
+++ src/main/kotlin/com/feb/mod/FebmodDataGenerator.kt
@@ -0,0 +1,9 @@
+package com.feb.mod
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator
+
+object FebmodDataGenerator : DataGeneratorEntrypoint {
+	override fun onInitializeDataGenerator(fabricDataGenerator: FabricDataGenerator) {
+	}
+}
